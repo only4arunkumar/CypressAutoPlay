8147f3824901e7f3fe30a4bbe675c6cfbab967eb
only4arunkumar <only4arunkumar@gmail.com>
2021-06-03 12:05:49 +1000

    Add files via upload
    Autoplay not happening for the Video

---
+++ Cypress_AutoPlay/cypress/integration/autoPlay.js
@@ -0,0 +1,13 @@
+describe("Check the Autoplay video are played without any action",()=>{
+
+    it("Should load the url with autoplay flag and start playing the video",()=>{
+
+        cy.visit("https://tester-test.vudoo.io/watch/61209085922?autoPlay=1")
+        //Ideally the video should autoplay and the  test should be able to find the  element with text click
+        //As the video is not autoplaying  the test is uanble to find the elemnent to be clicked
+        cy.enter("#vudoo-iframe-61209085922").then((iFrameBody)=>{
+          iFrameBody().findByText(/click/i).should("exist")
+        })
+        
+    })
+})
